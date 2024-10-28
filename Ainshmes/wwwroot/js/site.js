2a8b87ba8fa7bef60f379b90a5a57313831dc57b
Huwayda <huwayda2001saa@gmail.com>
2024-10-28 21:33:07 +0200

    Add project files.

---
+++ Ainshmes/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿//سهم الرجوع للاعلي
+// احصل على السهم
+const backToTopButton = document.getElementById('back-to-top');
+
+// عند التمرير، تحقق من المسافة
+window.onscroll = function () {
+    // إذا نزل المستخدم 100px أو أكثر، يظهر السهم
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// عند الضغط على السهم، عد إلى أعلى الصفحة
+backToTopButton.addEventListener('click', function (e) {
+    e.preventDefault(); // منع السلوك الافتراضي للرابط
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // للانتقال الناعم
+    });
+});
+//صور المعهد 
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
